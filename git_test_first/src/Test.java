9fc35600f1b602aa52f3f4418c764dc6afae69ea
lizhengpeng <972996066@qq.com>
2020-07-15 20:57:47 +0800

    提交

---
+++ git_test_first/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author lizhengpeng
+ * @create 2020/7/15 - 20:55
+ * @describe
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println( "111" );
+    }
+}

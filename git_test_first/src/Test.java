741067e07ed60f3a43c73272779be9ded0305536
lizhengpeng <972996066@qq.com>
2020-07-15 21:12:14 +0800

    测试

--- git_test_first/src/Test.java
+++ git_test_first/src/Test.java
@@ -7,6 +7,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println( "111" );
         String aa="lzp";
+        String a2="lzp2222";
+        a2=a2+"kkkk";
+
     }
 
     public void testpp(){

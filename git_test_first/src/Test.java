cfaffebc8664297fc39e107c2b6a84775ccc087f
lizhengpeng <972996066@qq.com>
2020-07-15 21:10:30 +0800

    提交

--- git_test_first/src/Test.java
+++ git_test_first/src/Test.java
@@ -7,6 +7,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println( "111" );
         String aa="lzp";
+        String a1="jiangt";
+        a1=a1+aa;
     }
 
     public void testpp(){
